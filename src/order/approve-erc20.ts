05b9f57e1c152e0496185bedbc7325aa30cf144a
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-07-20 14:30:09 +0300

    chore(*): approveErc20 doc

--- src/order/approve-erc20.ts
+++ src/order/approve-erc20.ts
@@ -8,7 +8,7 @@ import { sentTx } from "../common/send-transaction"
 const infiniteBn = toBn(2).pow(256).minus(1)
 
 export async function approveErc20(
-	web3: Web3, contract: Address, owner: Address, operator: Address, value: BigNumber | BN, infinite: Boolean = true,
+	web3: Web3, contract: Address, owner: Address, operator: Address, value: BigNumber | BN, infinite: boolean = true,
 ): Promise<string | undefined> {
 	const erc20 = createErc20Contract(web3, contract)
 	const allowance = toBn(await erc20.methods.allowance(owner, operator).call())

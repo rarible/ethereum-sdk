1ef4cb07530c66d257414308edc41ba0cdf4e2b2
vitaliyzhalnin <cueage@gmail.com>
2021-07-22 10:41:11 +0700

    RPN-653 refactor contract paran on approve function

--- src/order/approve.ts
+++ src/order/approve.ts
@@ -7,19 +7,23 @@ import { approveErc20 } from "./approve-erc20"
 import {approveErc721} from "./approve-erc721";
 import {approveErc1155} from "./approve-erc1155";
 import {sentTx} from "../common/send-transaction";
-import {getErc20TransferProxyAddress, getTransferProxyAddress} from "./addresses";
+import {
+	getErc1155LazyMintTransferProxy,
+	getErc20TransferProxyAddress,
+	getErc721LazyMintTransferProxy,
+	getTransferProxyAddress
+} from "./addresses";
 
 export async function approve(
 	web3: Web3,
-	contract: Address,
 	owner: Address,
 	asset: Asset,
 	infinite: undefined | boolean = true
 ): Promise<Action | undefined> {
 	const chainId = await web3.eth.getChainId()
-
 	switch (asset.assetType.assetClass) {
 		case "ERC20": {
+			const contract = asset.assetType.contract
 			const operator = getErc20TransferProxyAddress(chainId)
 			const action = async () => {
 				await approveErc20(sentTx, web3, contract, owner, operator, asset.value, infinite)
@@ -30,6 +34,7 @@ export async function approve(
 			}
 		}
 		case "ERC721": {
+			const contract = asset.assetType.contract
 			const operator = getTransferProxyAddress(chainId)
 			const action = async () => {
 				await approveErc721(sentTx, web3, contract, owner, operator)
@@ -40,6 +45,7 @@ export async function approve(
 			}
 		}
 		case "ERC1155": {
+			const contract = asset.assetType.contract
 			const operator = getTransferProxyAddress(chainId)
 			const action = async () => {
 				await approveErc1155(sentTx, web3, contract, owner, operator)
@@ -50,9 +56,26 @@ export async function approve(
 			}
 		}
 		case "ERC721_LAZY":
-			break
-		case "ERC1155_LAZY":
-			break
+			const contract = asset.assetType.contract
+			const operator = getErc721LazyMintTransferProxy(chainId)
+			const action = async () => {
+				await approveErc721(sentTx, web3, contract, owner, operator)
+			}
+			return{
+				name: 'approve-erc721-lazy',
+				value: action
+			}
+		case "ERC1155_LAZY": {
+			const contract = asset.assetType.contract
+			const operator = getErc1155LazyMintTransferProxy(chainId)
+			const action = async () => {
+				await approveErc1155(sentTx, web3, contract, owner, operator)
+			}
+			return {
+				name: 'approve-erc1155-lazy',
+				value: action
+			}
+		}
 	}
 	return undefined
 }

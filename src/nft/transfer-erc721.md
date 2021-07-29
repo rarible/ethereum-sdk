6bf7d7ebc51b1ffd4f055c653dbb84224a97c93f
vitaliyzhalnin <cueage@gmail.com>
2021-07-29 12:51:01 +0700

    RPN-721 add transfer function for erc721 token

---
+++ src/nft/transfer-erc721.md
@@ -0,0 +1,9 @@
+### transfer erc721
+
+Transfer erc721 token from sender to newOwner.
+
+Arguments:
+- contract: Address - address of the ERC20 contract
+- from: Address - owner of the ERC721 token
+- to: Address - new owner
+- tokenId: string - id of nft to transfer
---
+++ src/nft/transfer-erc721.test.ts
@@ -0,0 +1,59 @@
+import { randomAddress, toAddress } from "@rarible/types"
+import { Contract } from "web3-eth-contract"
+import { sentTx } from "../common/send-transaction"
+import { createGanacheProvider } from "../test/create-ganache-provider"
+import {deployTestErc721} from "../order/contracts/test-erc721";
+import {Address} from "@rarible/protocol-api-client";
+import {transferErc721} from "./transfer-erc20";
+
+describe("transfer Erc721", () => {
+    const { web3, address: testAddress } = createGanacheProvider()
+    let testErc721: Contract
+    let from = testAddress
+    let to = randomAddress()
+
+    beforeAll(async () => {
+        testErc721 = await deployTestErc721(web3, "TST", "TST")
+    })
+
+    test(`should transfer erc721 token from: ${from}, to: ${to}`, async () => {
+        const tokenId1: string = from + "b00000000000000000000001"
+        await testErc721.methods.mint(from, tokenId1, 'https://nft.com').send({ from, gas: 200000 })
+
+        const senderBalance = await testErc721.methods.balanceOf(from).call()
+        expect(senderBalance === '1').toBeTruthy()
+
+        const ownership: Address = await testErc721.methods.ownerOf(tokenId1).call()
+        expect(toAddress(ownership) === toAddress(from)).toBeTruthy()
+
+        await testErc721.methods.setApprovalForAll(to, true)
+        const isApproved: boolean = await testErc721.methods.isApprovedForAll(from, to)
+        expect(isApproved).toBeTruthy()
+
+        const hash = await transferErc721(sentTx, web3, toAddress(testErc721.options.address), from, to, tokenId1)
+        expect(!!hash).toBeTruthy()
+
+        const receiverOwnership = await testErc721.methods.ownerOf(tokenId1).call()
+        expect(toAddress(receiverOwnership) === toAddress(to)).toBeTruthy()
+
+        const resultSenderBalance = await testErc721.methods.balanceOf(testAddress).call()
+        expect(resultSenderBalance === '0').toBeTruthy()
+
+        const resultReceiverBalance = await testErc721.methods.balanceOf(to).call()
+        expect(resultReceiverBalance === '1').toBeTruthy()
+    })
+
+    test(`should throw ownership error`, async () => {
+        const tokenId2: string = from + "b00000000000000000000002"
+        await testErc721.methods.mint(from, tokenId2, 'https://nft.com').send({ from, gas: 200000 })
+        const senderBalance = await testErc721.methods.balanceOf(from).call()
+        expect(senderBalance === '1').toBeTruthy()
+
+        const ownership: Address = await testErc721.methods.ownerOf(tokenId2).call()
+        expect(toAddress(ownership) === toAddress(from)).toBeTruthy()
+
+        const hash = await transferErc721(sentTx, web3, toAddress(testErc721.options.address), randomAddress(), to, tokenId2)
+        expect(!!hash).toBeFalsy()
+    })
+
+})

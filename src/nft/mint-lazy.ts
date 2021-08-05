e1323dd0e1ff0e6b7f89a4344cca8783890d8af8
vitaliyzhalnin <cueage@gmail.com>
2021-08-05 11:04:29 +0700

    write some logic for lazyMint

--- src/nft/mint-lazy.ts
+++ src/nft/mint-lazy.ts
@@ -1,25 +1,84 @@
 import Web3 from "web3";
 import {
-    Erc1155LazyAssetType,
-    Erc721LazyAssetType,
+    Address, Binary, Erc1155AssetType,
+    Erc721AssetType,
+    NftCollectionControllerApi,
     NftLazyMintControllerApi
 } from "@rarible/protocol-api-client";
+import {BigNumber, toAddress} from "@rarible/types";
+import {toBigNumber} from "@rarible/types/build/big-number";
 
 type MintLazyRequest = {
-    assetType: Pick<Erc721LazyAssetType, 'assetClass' | 'royalties' | 'creators'> | Pick<Erc1155LazyAssetType, 'assetClass' | 'royalties' | 'creators'>
+    assetType: Pick<Erc721AssetType, 'assetClass'> | Pick<Erc1155AssetType, 'assetClass'>
+    minter: Address,
+    royalties: [],
+    creators: [],
+    supply?: BigNumber
 }
 
 export async function mintLazy(
     web3: Web3,
-    nftLazyMintController: NftLazyMintControllerApi,
+    signTypedData: (signer: string, data: any) => Promise<Binary>,
+    nftCollection: NftCollectionControllerApi,
+    nftLazyMintApi: NftLazyMintControllerApi,
     mintLazyRequest: MintLazyRequest
     ): Promise<any> {
 
-    // get contract address based on assetType
-    // request generate id
-    // request ipfs link
-    // sign typedData
-    // request nftLazyMintController.mintNftAsset
-
-    return ''
+    // todo get contract address based on assetType for Rarible collection or create new collection
+    const contract = '0x0'
+    const {tokenId} = await nftCollection.generateNftTokenId({collection: contract, minter: mintLazyRequest.minter})
+    // todo request ipfs link
+    const ipfsUri = "/ipfs/QmWLsBu6nS4ovaHbGAXprD1qEssJu4r5taQfB74sCG51tp"
+    switch (mintLazyRequest.assetType.assetClass) {
+        case "ERC721": {
+            const data = {
+                "@type": mintLazyRequest.assetType.assetClass,
+                contract,
+                tokenId,
+                uri: ipfsUri,
+                creators: mintLazyRequest.creators,
+                royalties: mintLazyRequest.royalties,
+            }
+            const signature = await signTypedData(mintLazyRequest.minter, data)
+            return await nftLazyMintApi.mintNftAsset({
+                lazyNft: {
+                    "@type": "ERC721",
+                    contract: toAddress(contract),
+                    tokenId: tokenId,
+                    uri: ipfsUri,
+                    creators: mintLazyRequest.creators,
+                    royalties: mintLazyRequest.royalties,
+                    signatures: [signature],
+                }
+            })
+        }
+        case "ERC1155": {
+            const data = {
+                "@type": mintLazyRequest.assetType.assetClass,
+                contract,
+                tokenId,
+                uri: ipfsUri,
+                creators: mintLazyRequest.creators,
+                royalties: mintLazyRequest.royalties,
+                supply: mintLazyRequest.supply,
+            }
+            const signature = await signTypedData(mintLazyRequest.minter, data)
+            if (mintLazyRequest.supply) {
+                return await nftLazyMintApi.mintNftAsset({
+                    lazyNft: {
+                        "@type": "ERC1155",
+                        contract: toAddress(contract),
+                        tokenId: tokenId,
+                        uri: ipfsUri,
+                        creators: mintLazyRequest.creators,
+                        royalties: mintLazyRequest.royalties,
+                        signatures: [signature],
+                        supply: toBigNumber(mintLazyRequest.supply)
+                    }
+                })
+            } else {
+                return Promise.reject("Error: property 'supply: BidNumber' for ERC1155 is required")
+            }
+        }
+    }
 }

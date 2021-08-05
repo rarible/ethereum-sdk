602304a2880e217b33751d54ee561c884ca2c833
vitaliyzhalnin <cueage@gmail.com>
2021-08-05 19:22:41 +0700

    RPN-750 siplify check-asset-type test

--- src/order/check-asset-type.test.ts
+++ src/order/check-asset-type.test.ts
@@ -5,6 +5,7 @@ import {createE2eProvider} from "../test/create-e2e-provider";
 import {deployTestErc721} from "./contracts/test/test-erc721";
 import {deployTestErc20} from "./contracts/test/test-erc20";
 import {Contract} from "web3-eth-contract";
+import {checkAssetType} from "./check-asset-type";
 
 
 describe("check-asset-type test", function() {
@@ -19,47 +20,32 @@ describe("check-asset-type test", function() {
     test("should set assetClass if type not present", async () => {
 
         await testErc721.methods.mint(wallet.getAddressString(), "1", "").send({ from: wallet.getAddressString(), gas: 200000 })
-        const order = await sdk.order.sell({
-            makeAssetType: {
-                contract: toAddress(testErc721.options.address),
-                tokenId: toBigNumber("1"),
-            },
-            amount: 1,
-            maker: toAddress(wallet.getAddressString()),
-            originFees: [],
-            payouts: [],
-            price: '10',
-            takeAssetType: { assetClass: "ERC20", contract: toAddress(testErc20.options.address) },
-            // takeAssetType: { assetClass: "ERC20", contract: toAddress(testErc20.options.address) },
-        }).then(a => a.runAll())
 
-        const orderFromApi = await sdk.apis.order.getOrderByHash({hash: order.hash})
-        expect(orderFromApi.maker).toEqual(wallet.getAddressString())
-        const collection = await sdk.apis.nftCollection.getNftCollectionById({collection: testErc721.options.address})
-        expect(collection.type).toEqual("ERC721")
+        const assetType = await checkAssetType(
+            sdk.apis.nftItem,
+            sdk.apis.nftCollection,
+            {
+                contract: toAddress(testErc721.options.address),
+                tokenId: toBigNumber("1")
+            }
+        )
+        expect(assetType.assetClass).toEqual("ERC721")
     }, 50000)
 
     test("should leave as is if assetClass present", async () => {
 
         await testErc721.methods.mint(wallet.getAddressString(), "2", "").send({ from: wallet.getAddressString(), gas: 200000 })
-        const order = await sdk.order.sell({
-            makeAssetType: {
+
+        const assetType = await checkAssetType(
+            sdk.apis.nftItem,
+            sdk.apis.nftCollection,
+            {
                 assetClass: 'ERC721',
                 contract: toAddress(testErc721.options.address),
-                tokenId: toBigNumber("2"),
-            },
-            amount: 1,
-            maker: toAddress(wallet.getAddressString()),
-            originFees: [],
-            payouts: [],
-            price: '10',
-            takeAssetType: { assetClass: "ERC20", contract: toAddress(testErc20.options.address) },
-        }).then(a => a.runAll())
-
-        const orderFromApi = await sdk.apis.order.getOrderByHash({hash: order.hash})
-        expect(orderFromApi.maker).toEqual(wallet.getAddressString())
-        const collection = await sdk.apis.nftCollection.getNftCollectionById({collection: testErc721.options.address})
-        expect(collection.type).toEqual("ERC721")
+                tokenId: toBigNumber("2")
+            }
+        )
+        expect(assetType.assetClass).toEqual("ERC721")
     }, 50000)
 
 })

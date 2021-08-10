f0d1e99844e0ab0af25ba5d5e2b9f32ff8dc6aa0
vitaliyzhalnin <cueage@gmail.com>
2021-08-10 20:06:56 +0700

    replace web3 to abstraction in approve func

--- packages/protocol-ethereum-sdk/src/order/approve-erc1155.ts
+++ packages/protocol-ethereum-sdk/src/order/approve-erc1155.ts
@@ -1,20 +1,18 @@
-import Web3 from "web3";
-import {Address} from "@rarible/protocol-api-client";
-import {createErc1155Contract} from "./contracts/erc1155";
-import {ContractSendMethod, SendOptions} from "web3-eth-contract";
+import { Address } from "@rarible/protocol-api-client"
+import { Ethereum } from "../../../ethereum-provider"
+import { createErc1155Contract } from "./contracts/erc1155"
 
 export async function approveErc1155(
-    sendTx: (source: ContractSendMethod, options: SendOptions) => Promise<string>,
-    web3: Web3,
-    contract: Address,
-    owner: Address,
-    operator: Address
+	ethereum: Ethereum,
+	contract: Address,
+	owner: Address,
+	operator: Address,
 ): Promise<string | undefined> {
-    const erc1155 = createErc1155Contract(web3, contract)
-    const allowance: boolean = await erc1155.methods.isApprovedForAll(owner, operator).call()
-    if (!allowance) {
-        const [address] = await web3.eth.getAccounts()
-        return sendTx(erc1155.methods.setApprovalForAll(operator, true), { from: address })
-    }
-    return undefined
+	const erc1155 = createErc1155Contract(ethereum, contract)
+	const allowance: boolean = await erc1155.call("isApprovedForAll", owner, operator)
+	if (!allowance) {
+		const tx = await erc1155.send("setApprovalForAll", operator, true)
+		return tx.hash
+	}
+	return undefined
 }

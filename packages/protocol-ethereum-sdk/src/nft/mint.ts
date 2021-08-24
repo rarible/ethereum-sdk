d58465bf6c88afc6e5337df9128a2e1c86d1a94f
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-08-24 17:21:01 +0300

    fix(*): new types for mint function

--- packages/protocol-ethereum-sdk/src/nft/mint.ts
+++ packages/protocol-ethereum-sdk/src/nft/mint.ts
@@ -1,6 +1,8 @@
 import {
 	Address,
 	Binary,
+	LazyErc1155,
+	NftCollection,
 	NftCollectionControllerApi,
 	NftLazyMintControllerApi,
 	Part,
@@ -9,6 +11,8 @@ import { Ethereum } from "@rarible/ethereum-provider"
 import { SimpleLazyNft } from "./sign-nft"
 import { mintOnChain } from "./mint-on-chain"
 import { mintOffChain } from "./mint-off-chain"
+import { LazyErc721 } from "@rarible/protocol-api-client/build/models/LazyNft"
+import { toBigNumber } from "@rarible/types/build/big-number"
 
 type SimpleNft721Legacy = {
 	"@type": "ERC721"
@@ -53,5 +57,99 @@ export async function mint(
 	}
 }
 
+type ERC721Collection = Pick<NftCollection, "id" | "features"> & { type: "ERC721" }
+type LegacyERC721Collection = ERC721Collection & { [lazySupported]: false }
+type LazyERC721Collection = ERC721Collection & { [lazySupported]: true }
+
+function isLazy721Collection(
+	collection: Pick<NftCollection, "id" | "type" | "features">,
+): collection is LazyERC721Collection {
+	return collection.type === "ERC721" && collection.features.indexOf("MINT_WITH_ADDRESS") !== -1
+}
+
+function isLegacyErc721Collection(
+	collection: Pick<NftCollection, "id" | "type" | "features">,
+): collection is LegacyERC721Collection {
+	return collection.type === "ERC721" && collection.features.indexOf("MINT_WITH_ADDRESS") === -1
+}
+
+type LegacyERC721Request = {
+	collection: LegacyERC721Collection
+	uri: string
+	royalties: Array<Part>
+}
+
+type LazyErc721Request = {
+	collection: LazyERC721Collection,
+	lazy?: boolean
+} & Omit<LazyErc721, "signatures" | "contract" | "tokenId" | "@type">
+
+type ERC1155Collection = Pick<NftCollection, "id" | "features"> & { type: "ERC1155" }
+type LegacyERC1155Collection = ERC1155Collection & { [lazySupported]: false }
+type LazyERC1155Collection = ERC1155Collection & { [lazySupported]: true }
+
+function isLazy1155Collection(
+	collection: Pick<NftCollection, "id" | "type" | "features">,
+): collection is LazyERC1155Collection {
+	return collection.type === "ERC1155" && collection.features.indexOf("MINT_WITH_ADDRESS") !== -1
+}
+
+function isLegacyErc1155Collection(
+	collection: Pick<NftCollection, "id" | "type" | "features">,
+): collection is LegacyERC1155Collection {
+	return collection.type === "ERC1155" && collection.features.indexOf("MINT_WITH_ADDRESS") === -1
+}
+
+type LegacyERC1155Request = {
+	collection: LegacyERC1155Collection
+	uri: string
+	supply: number
+	royalties: Array<Part>
+}
+
+type LazyErc1155Request = {
+	collection: LazyERC1155Collection,
+	lazy?: boolean
+} & Omit<LazyErc1155, "signatures" | "contract" | "tokenId" | "@type">
+
+function showHowToMint(collection: NftCollection) {
+	if (isLazy721Collection(collection)) {
+		newMint({
+			collection,
+			uri: "uri",
+			royalties: [],
+			creators: []
+		})
+	} else if (isLegacyErc721Collection(collection)) {
+		newMint({
+			collection,
+			uri: "",
+			royalties: []
+		})
+	} else if (isLazy1155Collection(collection)) {
+		newMint({
+			collection,
+			uri: "",
+			royalties: [],
+			supply: toBigNumber("1"),
+			creators: []
+		})
+	} else if (isLegacyErc1155Collection(collection)) {
+		newMint({
+			collection,
+			uri: "",
+			royalties: [],
+			supply: 1
+		})
+	}
+}
+
+declare const lazySupported: unique symbol
+
+type NewMintRequest = LazyErc721Request | LegacyERC721Request | LazyErc1155Request | LegacyERC1155Request
+
+function newMint(request: NewMintRequest) {
+
+}
 
 

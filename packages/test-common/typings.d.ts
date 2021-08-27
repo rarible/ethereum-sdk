4de791843db16effcf0a56c7612d5934b5a1e581
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-08-27 15:48:02 +0300

    fix(*): eslintignore

--- packages/test-common/typings.d.ts
+++ packages/test-common/typings.d.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line
 declare type GlobalFetch = WindowOrWorkerGlobalScope
 declare module "web3-provider-engine/subproviders/hooked-wallet"
 declare module "web3-provider-engine/subproviders/rpc"

2d1a79715bc969d9edb5caaf47922a59e7f7b12d
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-09-15 13:00:33 +0300

    chore(*): better jest config

--- packages/protocol-ethereum-sdk/jest.config.js
+++ packages/protocol-ethereum-sdk/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	roots: ["<rootDir>/src"],
-	setupFiles: ["<rootDir>/setup.js"],
+	setupFiles: ["<rootDir>/jest.setup.js"],
 	transform: {
 		"^.+\\.ts?$": "ts-jest",
 	},
--- packages/protocol-ethereum-sdk/jest.setup.js
+++ packages/protocol-ethereum-sdk/jest.setup.js
@@ -2,3 +2,4 @@ global.FormData = require("form-data")
 global.window = {
 	fetch: require("node-fetch"),
 }
+jest.setTimeout(10000)

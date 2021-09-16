48e0b5cb89f6fb8d87d0725581d12e48862b2715
Evgeny Nacu <evgeny.nacu@gmail.com>
2021-09-16 07:44:33 +0300

    fix(*): typed signature workaround for hardware wallets

--- packages/ethereum-provider/src/utils.ts
+++ packages/ethereum-provider/src/utils.ts
@@ -1,22 +1,35 @@
-import { SignTypedDataMethodEnum, MessageTypes, TypedMessage } from "./domain"
+import { TypedDataUtils } from "eth-sig-util"
+import { MessageTypes, SignTypedDataMethodEnum, TypedMessage } from "./domain"
 import type { Ethereum } from "."
 
 export async function signTypedDataInternal<T extends MessageTypes>(
-	signer: string, ethereum: Ethereum, data: TypedMessage<T>
+	ethereum: Ethereum, data: TypedMessage<T>,
 ): Promise<string> {
+	const signer = await ethereum.getFrom()
 	try {
 		return await ethereum.send(SignTypedDataMethodEnum.V4, [signer, JSON.stringify(data)])
 	} catch (error) {
 		console.error("got error white executing sign typed data v4", error)
-		filterErrors(error)
-		try {
-			return await ethereum.send(SignTypedDataMethodEnum.V3, [signer, JSON.stringify(data)])
-		} catch (error) {
-			console.error("got error white executing sign typed data v3", error)
+		if ("message" in error && error.message === "MetaMask Message Signature: Error: Not supported on this device") {
+			return signWithHardwareWallets(ethereum, data)
+		} else {
 			filterErrors(error)
-			return ethereum.send(SignTypedDataMethodEnum.DEFAULT, [signer, data])
+			try {
+				return await ethereum.send(SignTypedDataMethodEnum.V3, [signer, JSON.stringify(data)])
+			} catch (error) {
+				console.error("got error white executing sign typed data v3", error)
+				filterErrors(error)
+				return ethereum.send(SignTypedDataMethodEnum.DEFAULT, [signer, data])
+			}
 		}
 	}
+}
+
+async function signWithHardwareWallets<T extends MessageTypes>(ethereum: Ethereum, data: TypedMessage<T>) {
+	const hash = TypedDataUtils.sign(data)
+	const signature = toBuffer(await ethereum.ethSign(`0x${hash.toString("hex")}`))
+	signature.writeInt8(signature[64] + 4, 64)
+	return `0x${signature.toString("hex")}`
 }
 
 /*
@@ -28,7 +41,7 @@ export async function signTypedDataInternal<T extends MessageTypes>(
 
 function filterErrors(original: unknown) {
 	if (hasCode(original)) {
-		if ([4900, 4001, 4901, 4100].includes(original.code))  {
+		if ([4900, 4001, 4901, 4100].includes(original.code)) {
 			throw original
 		}
 	}
@@ -36,4 +49,12 @@ function filterErrors(original: unknown) {
 
 function hasCode(error: unknown): error is { code: number } {
 	return typeof error === "object" && error !== null && "code" in error
+}
+
+function toBuffer(hex: string) {
+	if (hex.startsWith("0x")) {
+		return Buffer.from(hex.substring(2), "hex")
+	} else {
+		return Buffer.from(hex, "hex")
+	}
 }

de1c32633bb56bf25b1a3c276137696c013edc82
vitaliyzhalnin <cueage@gmail.com>
2021-09-29 13:30:33 +0300

    fix package name|fix network list

--- packages/sdk-bundle/src/index.ts
+++ packages/sdk-bundle/src/index.ts
@@ -2,7 +2,7 @@ import { Web3Ethereum } from "@rarible/web3-ethereum"
 import Web3 from "web3"
 import { createRaribleSdk as raribleSdk } from "@rarible/protocol-ethereum-sdk"
 
-function createRaribleSdk(web3: any, env: "e2e" | "rinkeby" | "mainnet") {
+function createRaribleSdk(web3: any, env: "e2e" | "ropsten" | "rinkeby" | "mainnet") {
 	return raribleSdk(new Web3Ethereum({ web3 }), env)
 }
 

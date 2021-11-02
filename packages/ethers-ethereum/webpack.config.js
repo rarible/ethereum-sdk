d5384f44b29d6604d54e2c7f2b7deeafb3de28f0
koryakovtsev.ivan <beatzhitta@gmail.com>
2021-11-02 14:34:15 +0300

    Merge pull request #42
    * chore: add umd build
    
    * chore: fix build script
    
    * umd module update
    
    * umd build update
    
    * example file
    
    * Merge branch 'master' into umd-build

---
+++ packages/ethers-ethereum/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+	entry: "./build/index.js",
+	output: {
+		path: path.resolve(__dirname, "umd"),
+		filename: "rarible-ethers-ethereum.js",
+		library: {
+			name: "raribleEthersEthereum",
+			type: "umd",
+		},
+	},
+	resolve: {
+		fallback: {
+			"stream": require.resolve("stream-browserify"),
+			"process": require.resolve("process/browser"),
+		},
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			process: "process/browser",
+		}),
+	],
+	mode: "production",
+	optimization: {
+		minimize: true,
+	},
+}

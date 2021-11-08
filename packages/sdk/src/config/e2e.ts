8a73b63e5fc4afa439ef5cfe450e55c986f352cf
Aleksandr <a.sozinov@rarible.com>
2021-11-08 18:10:03 +0300

    createToken factories: e2e/rinkeby addresses

--- packages/sdk/src/config/e2e.ts
+++ packages/sdk/src/config/e2e.ts
@@ -26,9 +26,9 @@ export const e2eConfig: EthereumConfig = {
 		proxyRegistry: ZERO_ADDRESS,
 	},
 	factories: {
-		erc721: ZERO_ADDRESS,
-		erc721User: ZERO_ADDRESS,
-		erc1155: ZERO_ADDRESS,
-		erc1155User: ZERO_ADDRESS,
+		erc721: toAddress("0xcf5C9CBAA56f9A81C5dF7f9eE15EfA14F4B6138A"),
+		erc721User: toAddress("0x1EaE9588a911B4fb15A4784070bbED5a7Df73347"),
+		erc1155: toAddress("0x5B18C8eCA489bD0ef1aA00fE768ecc9b63597bbf"),
+		erc1155User: toAddress("0x9D4811d5bDE3Af4e7F4B2C73Ed757d0a5c52497a"),
 	},
 }

bf6304177abb7b1bbd147898c37c69ab9e2c59e5
Aleksandr <a.sozinov@rarible.com>
2022-01-27 13:16:24 +0300

    feat: mumbai dev config

--- packages/sdk/src/config/index.ts
+++ packages/sdk/src/config/index.ts
@@ -6,6 +6,7 @@ import { mainnetConfig } from "./mainnet"
 import type { EthereumConfig } from "./type"
 import { mumbaiConfig } from "./mumbai"
 import { polygonConfig } from "./polygon"
+import { mumbaiDevConfig } from "./mumbai-dev"
 
 const configDictionary: Record<EthereumNetwork, EthereumConfig> = {
 	e2e: e2eConfig,
@@ -13,6 +14,7 @@ const configDictionary: Record<EthereumNetwork, EthereumConfig> = {
 	rinkeby: rinkebyConfig,
 	mainnet: mainnetConfig,
 	mumbai: mumbaiConfig,
+	"mumbai-dev": mumbaiDevConfig,
 	polygon: polygonConfig,
 }
 

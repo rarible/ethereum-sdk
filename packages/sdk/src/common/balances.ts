51b87b78ce12fbe9c6beac2aee999913144e9e90
Aleksandr <a.sozinov@rarible.com>
2022-02-17 23:06:45 +0300

    chore: wallet from balances was removed

--- packages/sdk/src/common/balances.ts
+++ packages/sdk/src/common/balances.ts
@@ -1,32 +1,22 @@
 import type { Address } from "@rarible/types"
-import type { Ethereum } from "@rarible/ethereum-provider"
 import type {
 	Erc20AssetType,
 	EthAssetType,
 } from "@rarible/ethereum-api-client/build/models/AssetType"
 import type { BigNumberValue } from "@rarible/utils"
 import { BigNumber, toBn } from "@rarible/utils"
-import type { Maybe } from "@rarible/types/build/maybe"
 import type { RaribleEthereumApis } from "./apis"
 
 export type BalanceRequestAssetType = EthAssetType | Erc20AssetType
 
 export class Balances {
-	constructor(
-		private readonly ethereum: Maybe<Ethereum>,
-		private readonly apis: RaribleEthereumApis,
-		private readonly checkWalletChainId: () => Promise<boolean>,
-	) {
+	constructor(private readonly apis: RaribleEthereumApis) {
 		this.getBalance = this.getBalance.bind(this)
 	}
 
 	async getBalance(address: Address, assetType: BalanceRequestAssetType): Promise<BigNumberValue> {
-		await this.checkWalletChainId()
 		switch (assetType.assetClass) {
 			case "ETH": {
-				if (!this.ethereum) {
-					throw new Error("Wallet is undefined")
-				}
 				const ethBalance = await this.apis.balances.getEthBalance({owner: address})
 				return toBn(ethBalance.decimalBalance)
 					.div(new BigNumber(10).pow(18))

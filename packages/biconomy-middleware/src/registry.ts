00966be4556ca1bc442e2d7c063f3f6d09790262
a.bayramov <a.bayramly@corp.mail.ru>
2022-03-30 12:08:36 +0300

    feat: pass function signature to getMetadata

--- packages/biconomy-middleware/src/registry.ts
+++ packages/biconomy-middleware/src/registry.ts
@@ -11,10 +11,30 @@ export class Registry implements IContractRegistry {
 		this.registryData = await resp.json()
 	}
 
-	async getMetadata(address: string): Promise<ContractMetadata | undefined> {
+	async getMetadata(address: string, data?: string): Promise<ContractMetadata | undefined> {
 		if (!this.registryData) {
 			await this.fetchData()
 		}
-		return this.registryData[address.toLowerCase()]
+
+		const metadata = this.registryData[address.toLowerCase()]
+
+		if (Array.isArray(metadata?.allowedFunctions)) {
+			if (!data || !metadata.allowedFunctions.length) {
+				return undefined
+			}
+			const methodId = normalize(data)
+			const allowedFunctions = metadata.allowedFunctions.map(value => normalize(value))
+			if (allowedFunctions.includes(methodId)) {
+				return metadata
+			}
+			return undefined
+		}
+
+		return metadata
 	}
+}
+
+function normalize(signature: string): string {
+	const normalized = signature.startsWith("0x") ? signature.slice(0, 10) : `0x${signature.slice(0, 8)}`
+	return normalized.toLowerCase()
 }

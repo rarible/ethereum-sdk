a63bb339b8258885c291a675a446e86834da5eeb
vitalbadjo <cueage@gmail.com>
2022-04-27 14:29:00 +0530

    fix: rinkeby opensea config

--- packages/sdk/src/config/rinkeby.ts
+++ packages/sdk/src/config/rinkeby.ts
@@ -9,7 +9,7 @@ export const rinkebyConfig: EthereumConfig = {
 	exchange: {
 		v1: toAddress("0xda381535565b97640a6453fa7a1a7b161af78cbe"),
 		v2: toAddress("0xd4a57a3bD3657D0d46B4C5bAC12b3F156B9B886b"),
-		openseaV1: toAddress("0x0845A62DF1D99C2014e8a5dA6D2B79253Aa8319e"),
+		openseaV1: toAddress("0xdd54d660178b28f6033a953b0e55073cfa7e3744"),
 		bulkV2: toAddress("0x571a83b6F69A6DeE58af373e33762989b230431c"),
 	},
 	transferProxies: {
@@ -17,14 +17,14 @@ export const rinkebyConfig: EthereumConfig = {
 		erc20: toAddress("0x2fce8435f0455edc702199741411dbcd1b7606ca"),
 		erc721Lazy: toAddress("0x75fDbe19C2dc673384dDc14C9F453dB86F5f32E8"),
 		erc1155Lazy: toAddress("0x0cF0AAb68432a3710ECbf2f1b112a11cEe31a83C"),
-		openseaV1: toAddress("0xA8a80cC1582BA27aFE4e70E7dEcF087B41012966"),
+		openseaV1: ZERO_ADDRESS,
 		cryptoPunks: toAddress("0xfc2aa1b3365b8e0cac7a7d22fd7655e643792d17"),
 	},
 	feeConfigUrl: FEE_CONFIG_URL,
 	openSea: {
 		metadata: id32("RARIBLE"),
-		proxyRegistry: toAddress("0xf78EeB368ac95b8c9a9B3b4Af1FB7966b8791FD3"),
-		merkleValidator: toAddress("0xa16E9985A1EF736a7848406bfEDa9A146CF8Ba1f"),
+		proxyRegistry: ZERO_ADDRESS,
+		merkleValidator: toAddress("0x45b594792a5cdc008d0de1c1d69faa3d16b3ddc1"),
 	},
 	factories: {
 		erc721: toAddress("0x62e0BDC23435321adFf249d6f41e11AEee6486Cf"),

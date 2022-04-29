f8ab7916f9ea14e6c160d8279b2bc8a836df1325
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-04-29 12:51:18 +0400

    feat: cryptopunk wrapper, check wrapper contract address

--- packages/sdk/src/nft/cryptopunk-wrapper.ts
+++ packages/sdk/src/nft/cryptopunk-wrapper.ts
@@ -2,6 +2,7 @@ import type { EthereumTransaction } from "@rarible/ethereum-provider"
 import type { Maybe } from "@rarible/types/build/maybe"
 import type { Ethereum } from "@rarible/ethereum-provider"
 import type { Address } from "@rarible/types"
+import { ZERO_ADDRESS } from "@rarible/types"
 import type { SendFunction } from "../common/send-transaction"
 import { createCryptoPunksWrapperContract } from "./contracts/cryptoPunks/cryptopunk-wrapper"
 import { createCryptoPunksMarketContract } from "./contracts/cryptoPunks"
@@ -17,6 +18,10 @@ export async function approveForWrapper(
 	await checkWalletChainId()
 	if (!ethereum) {
 		throw new Error("Wallet undefined")
+	}
+
+	if (wrapperContractAddress === ZERO_ADDRESS) {
+		throw new Error("Wrapper contract address is not defined")
 	}
 
 	const marketContract = createCryptoPunksMarketContract(

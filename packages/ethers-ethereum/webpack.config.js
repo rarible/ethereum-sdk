feae5d68e66d2de31e46e5bc44fb12baf396a872
Aleksandr Sozinov <sanzeting@mail.ru>
2022-05-16 17:10:23 +0300

    fix: ethers provider fix (#157)
    fix: ethers provider fix

--- packages/ethers-ethereum/webpack.config.js
+++ packages/ethers-ethereum/webpack.config.js
@@ -14,10 +14,14 @@ module.exports = {
 	resolve: {
 		fallback: {
 			"stream": require.resolve("stream-browserify"),
+			"Buffer": require.resolve("buffer"),
 			"process": require.resolve("process/browser"),
 		},
 	},
 	plugins: [
+		new webpack.ProvidePlugin({
+			Buffer: ["buffer", "Buffer"],
+		}),
 		new webpack.ProvidePlugin({
 			process: "process/browser",
 		}),

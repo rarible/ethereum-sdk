0c08bf7419101cf9ca327e469f23269d55dbfd37
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-08-09 16:51:51 +0400

    feat: add support for x2y2 orders

--- packages/sdk/src/order/fill-order/x2y2.ts
+++ packages/sdk/src/order/fill-order/x2y2.ts
@@ -1,19 +1,15 @@
 import type { Maybe } from "@rarible/types/build/maybe"
 import type { Ethereum } from "@rarible/ethereum-provider"
 import type { EthereumTransaction } from "@rarible/ethereum-provider/src"
-import { toAddress, ZERO_ADDRESS } from "@rarible/types"
-import type { BigNumber } from "@rarible/types"
-import { BigNumber as BigNum } from "@rarible/utils"
-import type { Part } from "@rarible/ethereum-api-client"
-import { toBn } from "@rarible/utils"
+import { toAddress, toBigNumber, ZERO_ADDRESS } from "@rarible/types"
 import type { SimpleOrder, SimpleX2Y2Order } from "../types"
 import type { SendFunction } from "../../common/send-transaction"
 import type { EthereumConfig } from "../../config/type"
 import { createExchangeWrapperContract } from "../contracts/exchange-wrapper"
-import { encodePartToBuffer } from "../encode-data"
 import type { X2Y2OrderFillRequest } from "./types"
-import { X2Y2Utils } from "./x2y2-utils/get-order-sign"
 import { ExchangeWrapperOrderType } from "./types"
+import { X2Y2Utils } from "./x2y2-utils/get-order-sign"
+import { calcValueWithFees, originFeeValueConvert } from "./common/origin-fees-utils"
 
 export class X2Y2OrderHandler {
 	constructor(
@@ -38,6 +34,8 @@ export class X2Y2OrderHandler {
 			throw new Error("x2y2 supports max up to 2 origin fee value")
 		}
 
+		const { originFeeConverted, totalFeeBasisPoints } = originFeeValueConvert(request.originFees)
+
 		const x2y2Input = X2Y2Utils.getOrderSign({
 			sender: toAddress(await this.ethereum.getFrom()),
 			op: X2Y2Utils.SELL_OP,
@@ -46,8 +44,7 @@ export class X2Y2OrderHandler {
 			price: order.take.value,
 		})
 
-		const { originFeeConverted, totalFeeBasisPoints } = originFeeValueConvert(request.originFees)
-		const totalValue = calcValueWithFees(order.take.value, totalFeeBasisPoints)
+		const valueForSending = calcValueWithFees(toBigNumber(order.take.value), totalFeeBasisPoints)
 
 		return this.send(
 			wrapper.functionCall("singlePurchase", {
@@ -56,42 +53,11 @@ export class X2Y2OrderHandler {
 				amount: order.take.value,
 				data: x2y2Input,
 			}, originFeeConverted[0], originFeeConverted[1]),
-			{ value: totalValue.toString() }
+			{ value: valueForSending.toString() }
 		)
 	}
 
 	getBaseOrderFee() {
 		return this.getBaseOrderFeeConfig("X2Y2")
 	}
-}
-
-export function originFeeValueConvert(originFees?: Part[]): {
-	originFeeConverted: [BigNumber, BigNumber]
-	totalFeeBasisPoints: number,
-} {
-	if (originFees && originFees.length > 1) {
-		throw new Error("x2y2 supports max up to 2 origin fee value")
-	}
-
-	const originFeeConverted: [BigNumber, BigNumber] = [
-		encodePartToBuffer(originFees?.[0]),
-		encodePartToBuffer(originFees?.[1]),
-	]
-
-	const totalFeeBasisPoints = (originFees?.[0]?.value ?? 0) + (originFees?.[1]?.value ?? 0)
-
-	return {
-		originFeeConverted,
-		totalFeeBasisPoints,
-	}
-}
-
-function calcValueWithFees(value: BigNumber, feesBasisPoints: number): BigNum {
-	const feesValue = toBn(feesBasisPoints)
-		.dividedBy(10000)
-		.multipliedBy(value)
-		.integerValue(BigNum.ROUND_FLOOR)
-
-	const valueWithFees = feesValue.plus(value)
-	return valueWithFees
 }

cefdc7b6fa125df653792812deb033b7f76620ba
Sergei Vychegzhanin <rzcoder@gmail.com>
2022-09-09 13:40:42 +0400

    fix: replace on to once in event listeners

--- packages/web3-ethereum/src/utils/to-promises.ts
+++ packages/web3-ethereum/src/utils/to-promises.ts
@@ -3,12 +3,12 @@ import type { PromiEvent, TransactionReceipt } from "web3-core"
 export function toPromises(promiEvent: PromiEvent<any>) {
 	return {
 		hash: new Promise<string>((resolve, reject) => {
-			promiEvent.on("error", reject)
-			promiEvent.on("transactionHash", resolve)
+			promiEvent.once("error", reject)
+			promiEvent.once("transactionHash", resolve)
 		}),
 		receipt: new Promise<TransactionReceipt>((resolve, reject) => {
-			promiEvent.on("error", reject)
-			promiEvent.on("receipt", resolve)
+			promiEvent.once("error", reject)
+			promiEvent.once("receipt", resolve)
 		}),
 	}
 }

f761746029907943f627b1c80f4d5b76dec56d7d
Aleksandr Sozinov <sanzeting@mail.ru>
2022-10-26 19:41:42 +0300

    feat: getting fees error level (#209)
    feat: getting fees error level

--- packages/sdk/src/common/estimate-gas.ts
+++ packages/sdk/src/common/estimate-gas.ts
@@ -1,5 +1,4 @@
-import type { EthereumFunctionCall } from "@rarible/ethereum-provider"
-import type { EthereumEstimateGasOptions } from "@rarible/ethereum-provider/src"
+import type { EthereumEstimateGasOptions, EthereumFunctionCall } from "@rarible/ethereum-provider"
 import { LogsLevel } from "../types"
 import type { ILoggerConfig } from "./logger/logger"
 import { getErrorMessageString } from "./logger/logger"

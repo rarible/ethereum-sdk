f17a212093dc2eb4dbdf8b590e1644b7a69c023c
Aleksandr <a.sozinov@rarible.com>
2022-11-10 16:50:00 +0300

    fix: staging adresess

--- packages/sdk/src/config/polygon-staging.ts
+++ packages/sdk/src/config/polygon-staging.ts
@@ -10,7 +10,7 @@ export const stagingPolygonConfig: EthereumConfig = {
 		v1: ZERO_ADDRESS,
 		v2: toAddress("0x6aABb267a1c440CfB5C200Ebcd078Efa9249492A"),
 		openseaV1: ZERO_ADDRESS,
-		wrapper: toAddress("0x83Eba28e4047A4Ec4df302bBAa28C0f88887C08C"),
+		wrapper: toAddress("0xE63941f52ecbA57d731bEd5447cd5d392468Ba7F"),
 		x2y2: ZERO_ADDRESS,
 	},
 	transferProxies: {
@@ -35,8 +35,8 @@ export const stagingPolygonConfig: EthereumConfig = {
 		wrapperContract: ZERO_ADDRESS,
 	},
 	sudoswap: {
-		pairFactory: ZERO_ADDRESS,
-		pairRouter: ZERO_ADDRESS,
+		pairFactory: toAddress("0x31C827f06E10e4999eb88c193669d408eF597B3D"),
+		pairRouter: toAddress("0xbDC9d365aD6131D1078409521e2432b154439F05"),
 	},
 	weth: toAddress("0x85de069e16a42880c57b0D6451D6C770EC1D3Bf7"),
 	auction: ZERO_ADDRESS,
